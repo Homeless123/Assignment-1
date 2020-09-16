8b7446959f282c7160de5aab8c99d6f1c342584e
Homeless123 <65819667+Homeless123@users.noreply.github.com>
2020-09-16 16:49:26 +0200

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,7 +1,10 @@
+package day_2;
 
 public class Question1 {
 	public static void main(String[] args) {
-		
-		System.out.println("Ahmet");
+		System.out.println("I'm ready to be a Java pro:)");
 	}
+	
+
 }
+ 
